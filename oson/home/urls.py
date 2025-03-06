35b696b9b155e73399c4a1414772fbab3b8c029c
Administrator <humoyunmahmudjinov@gmail.com>
2025-03-06 19:49:54 +0500

    cruds added

--- oson/home/urls.py
+++ oson/home/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home, infopill, about, contact
+from .views import Home, Infopill, About, Contact
 urlpatterns = [
-    path('', home, name='home'),
-    path('infopill/<int:pk>/', infopill, name='infopill'),
-    path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
+    path('', Home.as_view(), name='home'),
+    path('infopill/<int:pk>/', Infopill.as_view(), name='infopill'),
+    path('about/', About.as_view(), name='about'),
+    path('contact/', Contact.as_view(), name='contact'),
 ]
--- oson/home/views.py
+++ oson/home/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 from .models import Pills
+from django.views import View
+
+
 # Create your views here.
-def home(request):
-    return render(request, template_name='home.html', context={'pill':Pills.objects.all()})
+# def home(request):
+#     return render(request, template_name='home.html', context={'pill':Pills.objects.all()})
 
-def infopill(request, pk):
-    return render(request, template_name='infopill.html', context={'pill':Pills.objects.get(id=pk)})
+class Home(View):
+    def get(self, request):
+        return render(request, template_name='home.html', context={'pill':Pills.objects.all()})
 
-def about(request):
-    return render(request, template_name='about.html')
+# def infopill(request, pk):
+#     return render(request, template_name='infopill.html', context={'pill':Pills.objects.get(id=pk)})
 
-def contact(request):
-    return render(request, template_name='contact.html')
+class Infopill(View):
+    def get(self, request, pk):
+        return render(request, template_name='infopill.html', context={'pill':Pills.objects.get(id=pk)})
+
+# def about(request):
+#     return render(request, template_name='about.html')
+
+class About(View):
+    def get(self, request):
+        return render(request, template_name='about.html')
+
+# def contact(request):
+#     return render(request, template_name='contact.html')
+
+class Contact(View):
+    def get(self, request):
+        return render(request, template_name='contact.html')
